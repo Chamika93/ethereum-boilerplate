7a3d5f9e8911099acec02bf355199f64c027bb8b
ChamikaAnd <chamika.adikari@and.digital>
2022-01-03 18:57:18 +0000

    show nfts

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Account from "components/Account/Account";
 import Chains from "components/Chains";
-import TokenPrice from "components/TokenPrice";
-import ERC20Balance from "components/ERC20Balance";
-import ERC20Transfers from "components/ERC20Transfers";
-import DEX from "components/DEX";
-import NFTBalance from "components/NFTBalance";
-import Wallet from "components/Wallet";
-import { Layout, Tabs } from "antd";
+import  NFTBalance from "components/NFTBalance";
+import NFTTokenIds from "components/NFTTokenIds"
+import { Layout } from "antd";
 import "antd/dist/antd.css";
 import NativeBalance from "components/NativeBalance";
 import "./style.css";
-import QuickStart from "components/QuickStart";
-import Contract from "components/Contract/Contract";
+import Contract from "components/Contract/Contract"; 
+import SearchCollections from "components/SearchCollections";
 import Text from "antd/lib/typography/Text";
-import Ramper from "components/Ramper";
 import MenuItems from "./components/MenuItems";
 const { Header, Footer } = Layout;
 
@@ -52,6 +47,7 @@ const styles = {
 };
 const App = ({ isServerInfo }) => {
   const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } = useMoralis();
+  const [inputValue, setInputValue] = useState('explore');
 
   useEffect(() => {
     const connectorId = window.localStorage.getItem("connectorId");
@@ -64,15 +60,10 @@ const App = ({ isServerInfo }) => {
       <Router>
         <Header style={styles.header}>
           <Logo />
+          <SearchCollections setInputValue={setInputValue} />
           <MenuItems />
           <div style={styles.headerRight}>
             <Chains />
-            <TokenPrice
-              address="0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
-              chain="eth"
-              image="https://cloudflare-ipfs.com/ipfs/QmXttGpZrECX5qCyXbBQiqgQNytVGeZW5Anewvh2jc4psg/"
-              size="40px"
-            />
             <NativeBalance />
             <Account />
           </div>
@@ -80,49 +71,17 @@ const App = ({ isServerInfo }) => {
 
         <div style={styles.content}>
           <Switch>
-            <Route exact path="/quickstart">
-              <QuickStart isServerInfo={isServerInfo} />
-            </Route>
-            <Route path="/wallet">
-              <Wallet />
-            </Route>
-            <Route path="/1inch">
-              <Tabs defaultActiveKey="1" style={{ alignItems: "center" }}>
-                <Tabs.TabPane tab={<span>Ethereum</span>} key="1">
-                  <DEX chain="eth" />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={<span>Binance Smart Chain</span>} key="2">
-                  <DEX chain="bsc" />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={<span>Polygon</span>} key="3">
-                  <DEX chain="polygon" />
-                </Tabs.TabPane>
-              </Tabs>
-            </Route>
-            <Route path="/erc20balance">
-              <ERC20Balance />
-            </Route>
-            <Route path="/onramp">
-              <Ramper />
-            </Route>
-            <Route path="/erc20transfers">
-              <ERC20Transfers />
-            </Route>
+        
             <Route path="/nftBalance">
               <NFTBalance />
             </Route>
-            <Route path="/contract">
-              <Contract />
+            <Route path="/nftMarket">
+              <NFTTokenIds inputValue={inputValue}/>
             </Route>
-            <Route path="/">
-              <Redirect to="/quickstart" />
+            <Route path="/transactions">
+              <NFTBalance />
             </Route>
-            <Route path="/ethereum-boilerplate">
-              <Redirect to="/quickstart" />
-            </Route>
-            <Route path="/nonauthenticated">
-              <>Please login using the "Authenticate" button</>
-            </Route>
+          
           </Switch>
         </div>
       </Router>
--- src/components/NFTTokenIds.jsx
+++ src/components/NFTTokenIds.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { useMoralis, useNFTBalances } from "react-moralis";
+import { useMoralis } from "react-moralis";
 import { Card, Image, Tooltip, Modal, Input, Skeleton } from "antd";
 import { FileSearchOutlined, SendOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import { getExplorer } from "helpers/networks";
 import AddressInput from "./AddressInput";
 import { useVerifyMetadata } from "hooks/useVerifyMetadata";
+import { useNFTTokenIds } from '../hooks/useNFTTokenIds';
 
 const { Meta } = Card;
 
@@ -21,8 +22,8 @@ const styles = {
   },
 };
 
-function NFTBalance() {
-  const { data: NFTBalances } = useNFTBalances();
+function NFTTokenIds({inputValue}) {
+  const { NFTTokenIds, error, isLoading } = useNFTTokenIds(inputValue);
   const { Moralis, chainId } = useMoralis();
   const [visible, setVisibility] = useState(false);
   const [receiverToSend, setReceiver] = useState(null);
@@ -46,7 +47,6 @@ function NFTBalance() {
     setIsPending(true);
     await Moralis.transfer(options)
       .then((tx) => {
-        console.log(tx);
         setIsPending(false);
       })
       .catch((e) => {
@@ -64,14 +64,12 @@ function NFTBalance() {
     setAmount(e.target.value);
   };
 
-  console.log("NFTBalances", NFTBalances);
   return (
     <div style={{ padding: "15px", maxWidth: "1030px", width: "100%" }}>
-      <h1>🖼 NFT Balances</h1>
       <div style={styles.NFTs}>
-        <Skeleton loading={!NFTBalances?.result}>
-          {NFTBalances?.result &&
-            NFTBalances.result.map((nft, index) => {
+        <Skeleton loading={!NFTTokenIds}>
+          {NFTTokenIds &&
+            NFTTokenIds.map((nft, index) => {
               //Verify Metadata
               nft = verifyMetadata(nft);
               return (
@@ -125,4 +123,4 @@ function NFTBalance() {
   );
 }
 
-export default NFTBalance;
+export default NFTTokenIds;
